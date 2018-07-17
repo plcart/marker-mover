10014fecbb47856aa24ed98949ccd7e198701f40
Arthur Ferreira <Arthur.Ferreira@fleetmatics.com>
2018-07-17 14:52:22 +0100

    gh-pages content

---
+++ src/index.js
@@ -0,0 +1,30 @@
+/* jshint esversion:6*/
+/* global google*/
+
+(function(){
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center:new google.maps.LatLng(-25.363, 131.044)
+    });
+
+    const image = {
+        url: '/img/psyduck.png',
+        size: new google.maps.Size(72, 72),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(36, 36) 
+    };
+
+    const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(-25.363, 131.044),
+        map: map,
+        icon: image
+    });
+
+    map.addListener('click',(event)=>{
+        marker.animateTo({
+            latLng: event.latLng,
+            duration: 2
+        });
+    });
+
+})();
