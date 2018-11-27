4d26c7ae9154551849899e119261772eee7f671d
Arthur Ferreira <Arthur.Ferreira@fleetmatics.com>
2018-11-27 16:00:34 +0000

    moved to requestAnimationFrame

--- src/marker.extension.ts
+++ src/marker.extension.ts
@@ -3,32 +3,34 @@ declare var google;
 export interface MarkerAnimation {
     latLng: any;
     duration: number;
-    framesPerSecond?: number
 }
 
-google.maps.Marker.prototype.isOngoingAnimation = false;
-google.maps.Marker.prototype.locationBacklog = [];
-google.maps.Marker.prototype.animateTo = function ({ latLng, duration, framesPerSecond = 60 }: MarkerAnimation) {
-    if (this.isOngoingAnimation) {
-        this.locationBacklog.push({ latLng, duration, framesPerSecond });
+google.maps.Marker.prototype.playing = false;
+google.maps.Marker.prototype.queue = [];
+google.maps.Marker.prototype.step = 0;
+google.maps.Marker.prototype.iterator = 0;
+google.maps.Marker.prototype.steps = [];
+google.maps.Marker.prototype.curPos = null;
+google.maps.Marker.prototype.animationFrame = function (timespan) {
+    this.setPosition(this.steps.shift());
+    if (this.steps.length !== 0)
+        requestAnimationFrame(this.animationFrame);
+    else {
+        this.playing = false;
+        if (this.queue.length != 0)
+            this.animateTo(this.queue.shift());
+    }
+}
+google.maps.Marker.prototype.animateTo = function ({ latLng, duration }: MarkerAnimation) {
+    if (this.playing) {
+        this.queue.push({ latLng, duration });
     } else {
-        this.isOngoingAnimation = true;
-        const fps = framesPerSecond || 60;
-        const totalFrames = fps * duration;
-        const iteratorValue = 1 / totalFrames;
-        const currentPos = this.getPosition();
-        let currentValue = 0;
-        const interval = setInterval(() => {
-            var newPosition = google.maps.geometry.spherical.interpolate(currentPos, latLng, currentValue);
-            this.setPosition(newPosition);
-            currentValue += iteratorValue;
-            if (currentValue >= 1) {
-                this.isOngoingAnimation = false;
-                clearInterval(interval);
-                const nextLocation = this.locationBacklog.shift();
-                if(nextLocation)
-                    this.animateTo(nextLocation);
-            }
-        }, 1000 / fps);
+        this.playing = true;
+        this.step = (100 / (duration * 6000));
+        this.iterator = 0;
+        this.curPos = this.getPosition()
+        this.steps = Array.apply(null, { length: duration * 60 }).map((i) => google.maps.geometry.spherical.interpolate(this.curPos, latLng, this.iterator += this.step));
+        if (this.steps.length !== 0)
+            requestAnimationFrame(this.animationFrame);
     }
 }
